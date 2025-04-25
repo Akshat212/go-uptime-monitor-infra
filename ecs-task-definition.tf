8c86a49c9967c5b09f3b1b8a68f431f32c1cd212
Akshat Upadhyay <akshatrko@gmail.com>
2025-04-25 17:20:28 +0530

    Adding task definition and mapping the cluster

---
+++ ecs-task-definition.tf
@@ -0,0 +1,21 @@
+resource "aws_ecs_task_definition" "go_monitoring_task" {
+  family = "go-monitoring-task"
+  requires_compatibilities = [ "FARGATE" ]
+  network_mode = "awsvpc"
+  cpu = "256"
+  memory = "512"
+
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions = jsonencode([
+    {
+        name = "go-monitoring-cluster"
+        image = "351933853465.dkr.ecr.ap-south-1.amazonaws.com/go-uptime-monitor-app:latest"
+        portMappings = [
+            {
+                containerPort = 8080
+                hostPort = 8080
+            }
+        ]
+    }
+  ])
+}
