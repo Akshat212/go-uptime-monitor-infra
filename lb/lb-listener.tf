27cfc680b8ed4d19bb0566c47a0316bb1e14854c
Akshat Upadhyay <akshatrko@gmail.com>
2025-05-23 22:35:14 +0530

    Corrected the protocol in lb to https

--- lb/lb-listener.tf
+++ lb/lb-listener.tf
@@ -1,7 +1,7 @@
 resource "aws_lb_listener" "app_listener" {
   load_balancer_arn = aws_alb.herbs-alb.arn
   port = 443
-  protocol = "HTTP"
+  protocol = "HTTPS"
   ssl_policy = "ELBSecurityPolicy-2016-08"
   certificate_arn = var.acm_cert_arn
 
